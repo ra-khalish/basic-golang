14bdc041cc0d621dee1244af5339c398457a1473
rakhalish <rafidev30@gmail.com>
2021-10-29 07:31:35 +0700

    hello world

---
+++ hello-world/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	/*
+	komentar kode
+	menampilkan pesan hello world
+	*/
+	fmt.Println("Hello World", "Lu'")
+
+	// fmt.Println("baris ini tidak akan di eksekusi")
+}
+
