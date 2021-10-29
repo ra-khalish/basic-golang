7a1f1301da02c3918b0145b621fe35319b1ff75a
rakhalish <rafidev30@gmail.com>
2021-10-29 07:58:02 +0700

    operator

---
+++ operator/operator_aritmatika.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	/*
+	+ , penjumlahan
+	- , pengurangan
+	* , perkalian
+	/ , pembagian
+	% , modulus / sisa hasil pembagian
+	*/
+	var value = (((2+6) % 3) * 4-2)/3
+	fmt.Printf("hasil: %d\n", value)
+}
