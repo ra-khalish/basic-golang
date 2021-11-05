0c1ae5465a7bb56fcb44677b29b2b750cd27f51c
rakhalish <rafidev30@gmail.com>
2021-11-05 13:02:12 +0700

    function as parameter

---
+++ fungsi-sebagai-parameter/penerapan_fungsi_sebagai_parameter.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "strings"
+
+func filter(data []string, callback func(string) bool) []string {
+	var result []string
+	for _, each := range data {
+		if filtered := callback(each); filtered {
+			result = append(result, each)
+		}
+	}
+	return result
+}
+
+func main(){
+	var data = []string{"wick", "jason", "ethan"}
+	var dataContainsO = filter(data, func(each string) bool {
+		return strings.Contains(each, "o")
+	})
+	var dataLength5 = filter(data, func(each string) bool {
+		return len(each) == 5
+	})
+
+	fmt.Println("data asli \t\t:", data)
+	fmt.Println("filter ada huruf \"o\"\t:", dataContainsO)
+	fmt.Println("filter jumlah huruf \"5\"\t:", dataLength5)
+}
